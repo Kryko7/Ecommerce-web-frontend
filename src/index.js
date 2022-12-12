12fc2871e92af17047f9f6e6a49df22711c09c2f
Kryko7 <61829993+Kryko7@users.noreply.github.com>
2022-12-12 23:57:09 +0530

    Added Models
    Added model pages for sign in/ sign up and checkout

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SignIn from './models/SignIn';
+import SignUp from './models/SignUp';
+import Checkout from './models/Checkout';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Checkout />
   </React.StrictMode>
 );
 
